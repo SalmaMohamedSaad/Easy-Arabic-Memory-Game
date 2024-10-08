1f224ebfd678bf1ac9f8db4295da4aee802a2acd
SalmaMohamedSaad <salmaabouamod@gmail.com>
2024-10-08 20:53:22 +0300

    The second card was added and. Each card can be flipped alone. the text of each card can be evaluated and if it matches it display a message saying it's a match

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 /*-------------------------------- Constants --------------------------------*/
-const card = document.querySelector('.card')
+const cards = document.querySelectorAll('.card')
 const message = document.querySelector('#message')
+const matchMessage = document.querySelector('#matchMessage')
 const startBtn = document.querySelector('#start')
 const resetBtn = document.querySelector('#reset')
 //console.log(message.innerHTML)
 /*---------------------------- Variables (state) ----------------------------*/
-let cardText
+let cardText = []
+let firstCardValue
 /*------------------------ Cached Element References ------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
@@ -20,48 +22,95 @@ const showBtnById = (id) => {
   btn.style.display = 'block'
 }
 /*
-* Function flipCard will be called by either Start button or onClick event of the card
-* When Called by start button it will do the following:
+* Function flipCardsOnFront will be called by the onClick event of the card to do the following :
+1. Check if there is another card text displayed or not
+2. If another card is displayed then it will capture its text in a temp value
+3. Show the clicked card text and compare it to the temp value
+4. if the two values matches it will show a green message saying success and leave the cards text displayed as it is.
+5. if the two values do not match it will flip them back and store their text in the cardText array
+
+*/
+const flipCardsOnFront = (event) => {
+  //console.log(cards[0])
+
+  cards.forEach((card) => {
+    if (card.innerHTML) return
+    if (event.target.id === card.id) {
+      //console.log(cardText)
+      for (let i = 0; i < cardText.length; i++) {
+        if (cardText[i].cardId === event.target.id) {
+          card.innerHTML = cardText[i].cardText
+          card.style.background = 'yellow'
+          if (!firstCardValue) {
+            firstCardValue = cardText[i].cardText
+          } else {
+            if (firstCardValue === card.innerHTML) {
+              matchMessage.innerHTML = 'Its a Match!'
+              //card.style.background = 'green'
+            } else {
+            }
+          }
+        }
+      }
+    }
+    // if (card.innerHTML) {
+    //   cardText = card.innerHTML
+    //   card.innerHTML = ''
+    //   card.classList.remove('flipped')
+    //   card.style.background = 'red'
+    //   hideBtnById('start')
+    //   showBtnById('reset')
+    //   message.innerHTML = 'Game On!'
+    // } else {
+    //   for (let i = 0; i < cardText.length; i++) {
+    //     card.innerHTML = cardText
+    //   }
+    //   card.style.background = '#40e0d0'
+    // }
+  })
+
+  //console.log(cardText)
+}
+/*
+* Function startGame will be called by start button to do the following:
 1. preserve the card text in the variable cardText
 2. remove the text from the card as if it was flipped on its back
 3. change the backGround color of the card
 4. hide the start button by calling the function hideBtnById
 5. show the reset button by calling the function showBtnById
-* When called by clicking the card it will set the card inner text value back to the cardText variable, and set the card background property back to the front color
-
 */
-const flipCard = (event) => {
-  //console.log(event.target.id)
-
-  if (event.target.id === 'start' || card.innerHTML) {
-    cardText = card.innerHTML
+const startGame = (event) => {
+  cards.forEach((card) => {
+    cardText.push({ cardId: card.id, cardText: card.innerHTML })
     card.innerHTML = ''
     card.classList.remove('flipped')
     card.style.background = 'red'
     hideBtnById('start')
     showBtnById('reset')
     message.innerHTML = 'Game On!'
-  } else {
-    for (let i = 0; i < cardText.length; i++) {
-      card.innerHTML = cardText
-    }
-
-    card.style.background = '#40e0d0'
-  }
-
-  //console.log(cardText)
+  })
 }
-const startGame = () => {}
 const resetCard = () => {
-  card.innerHTML = cardText
-  card.style.background = '#40e0d0'
+  cards.forEach((card) => {
+    for (let i = 0; i < cardText.length; i++) {
+      if (cardText[i].cardId === card.id) {
+        card.innerHTML = cardText[i].cardText
+        card.style.background = 'green'
+        matchMessage.innerHTML = ''
+      }
+    }
+    hideBtnById('start')
+    showBtnById('reset')
+    message.innerHTML = `Let's Start!`
+  })
 
   hideBtnById('reset')
   showBtnById('start')
   message.innerHTML = `Let's Start!`
 }
 /*----------------------------- Event Listeners -----------------------------*/
-startBtn.addEventListener('click', flipCard)
+startBtn.addEventListener('click', startGame)
 resetBtn.addEventListener('click', resetCard)
-
-card.addEventListener('click', flipCard)
+cards.forEach((card) => {
+  card.addEventListener('click', flipCardsOnFront)
+})
